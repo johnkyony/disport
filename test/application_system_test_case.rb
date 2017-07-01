82211ca6569730131d8ce71322a419d6086e91d2
john  kyony <jkyony@gmail.com>
2017-07-01 20:00:29 +0000

    the gamificaton bug

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,4 +4,12 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :webkit
   include Warden::Test::Helpers
   Warden.test_mode!  
+  Capybara::Webkit.configure do |config|
+   config.allow_url("apis.google.com")
+   config.allow_url("https://ssl.gstatic.com/*")
+   config.allow_url("khms1.googleapis.com")
+   config.allow_url("https://www.gstatic.com/*")
+   config.allow_url("*://khms0.googleapis.com/*")
+
+  end
 end
