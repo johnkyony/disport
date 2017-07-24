6d5a3d113b6e43fa2598334c2c972cb9654693da
jkyony <jkyony@gmail.com>
2017-07-24 19:29:27 +0200

    the header consistency fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,10 @@ class ApplicationController < ActionController::Base
 
   def user_point 
     if user_signed_in?
-     @user_point = Point.find_by(user_id: current_user.id)
-     if @user_point.nil?
-       @user_point = Point.create(user_id: current_user.id , kind_id: 1 , value: 5 )
+     user_point = Point.where(user_id: current_user.id)
+     @user_calculated_points = user_point.sum(:value)
+     if user_point.nil?
+       user_point = Point.create(user_id: current_user.id , kind_id: 1 , value: 5 )
        flash[:success] = "First time login"
      end
     end	    
